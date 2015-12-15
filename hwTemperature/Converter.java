112ab141a4e9d33502aac460d54fed561e747166
Kevin Zhang <kevinzhang3702@gmail.com>
2015-12-15 17:35:59 -0500

    Wrote the formulas for conversion

--- hwTemperature/Converter.java
+++ hwTemperature/Converter.java
@@ -1,4 +1,14 @@
-public class Converter{
-    
+import javax.swing.*;
+import java.awt.*;
 
+public class Converter{ 
+    public static double FtoC(double input){
+	return (input - 32) * (5.0 / 9.0);
+    }
+
+    public static double CtoF(double input){
+	return (input * (9.0 / 5.0) + 32);
+    }
+
+    
 }
