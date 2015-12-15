0929736005ee193d5e629585831a2345cee904d1
Kevin Zhang <kevinzhang3702@gmail.com>
2015-12-15 17:38:09 -0500

    Additional changes

--- hwTemperature/Converter.java
+++ hwTemperature/Converter.java
@@ -1,13 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Converter{ 
+public class Converter extends JFrame implements ActionListener{ 
     public static double FtoC(double input){
 	return (input - 32) * (5.0 / 9.0);
     }
 
     public static double CtoF(double input){
 	return (input * (9.0 / 5.0) + 32);
+    }
+
+    public static void main(String[]args){
+	
+	
+
     }
 
     
